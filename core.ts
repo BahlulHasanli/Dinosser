4f50ed2823c03689fc5a8af26a96e81b8fa879c7
Bahlul Hasanli <behlulheseni@gmail.com>
2024-08-02 16:53:20 +0400

    Update
    1. Automatic folder creation if folders do not exist.
    
    2. The .env file has been created and the configs have been added to it and it is now possible to change the folders and the quality of the image file from there

---
+++ core.ts
@@ -0,0 +1,75 @@
+import { ensureDirSync, existsSync } from 'fsLib';
+import sharp from 'npm:sharp';
+
+export const FOLDER: {
+  CURRENT: string;
+  MAIN_FOLDER: string;
+  FROM_DIR: string;
+  TO_DIR: string;
+} = {
+  CURRENT: String(Deno.cwd()),
+  MAIN_FOLDER: String(Deno.env.get('MAIN_FOLDER')),
+  FROM_DIR: String(Deno.env.get('FROM_DIR')),
+  TO_DIR: String(Deno.env.get('TO_DIR')),
+};
+
+export function Run({
+  name,
+  fileSize,
+  mimeType,
+}: {
+  name: string;
+  fileSize: number;
+  mimeType: 'jpeg' | 'png' | 'webp';
+}) {
+  sharp(`${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.FROM_DIR}\\${name}`)
+    [mimeType]({
+      quality: Number(Deno.env.get('QUALITY')),
+    })
+    .toFile(
+      `${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.TO_DIR}\\${name}`,
+      (err, info) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // File details
+          console.log('%c=====================', 'color: white');
+          console.log(`%cFile name: ${name}`, 'background-color: green');
+          console.log(
+            `%cOld size: ${formatFileSize(fileSize)}`,
+            'background-color: darkorange'
+          );
+          console.log(
+            `%cNew size: ${formatFileSize(info.size)}`,
+            'background-color: darkgreen'
+          );
+          console.log('%c=====================', 'color: white');
+        }
+      }
+    );
+}
+
+export function isFolders() {
+  if (!existsSync(FOLDER.MAIN_FOLDER)) {
+    // Create folders
+    ensureDirSync(
+      `${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.FROM_DIR}`
+    );
+    ensureDirSync(`${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.TO_DIR}`);
+  }
+
+  return true;
+}
+
+export function formatFileSize(bytes: number): string {
+  const units: Array<string> = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let size: number = bytes;
+  let unitIndex: number = 0;
+
+  while (size >= 1024 && unitIndex < units.length - 1) {
+    size /= 1024;
+    unitIndex++;
+  }
+
+  return `${size.toFixed(2)} ${units[unitIndex]}`;
+}
--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
-import Run from './sharp.ts';
+import { FOLDER, isFolders, Run } from './core.ts';
 
-for await (const row of Deno.readDir(String(Deno.env.get('FROM_DIR')))) {
-  const mimeType = row?.name.split('.')[1];
+if (isFolders()) {
+  for await (const { name } of Deno.readDir(
+    `${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.FROM_DIR}`
+  )) {
+    const mimeType = name.split('.')[1];
+    const fileSize: number = Deno.statSync(
+      `${FOLDER.CURRENT}\\${FOLDER.MAIN_FOLDER}\\${FOLDER.FROM_DIR}\\${name}`
+    )?.size;
 
-  switch (mimeType) {
-    case 'jpeg':
-    case 'jpg':
-      Run(row.name, 'jpeg');
-      break;
-    case 'png':
-      Run(row.name, 'png');
-      break;
-    case 'webp':
-      Run(row.name, 'webp');
-      break;
+    switch (mimeType) {
+      case 'jpeg':
+      case 'jpg':
+        Run({
+          name,
+          fileSize,
+          mimeType: 'jpeg',
+        });
+        break;
+      case 'png':
+        Run({
+          name,
+          fileSize,
+          mimeType,
+        });
+        break;
+      case 'webp':
+        Run({
+          name,
+          fileSize,
+          mimeType,
+        });
+        break;
+    }
   }
 }
